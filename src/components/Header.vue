6539bc635d9b6ba352721686f962ca51d1d6053f
payose <inipayose@gmail.com>
2021-09-21 13:30:30 +0100

    sidebar, trending and discover movies

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav id="header" class="navbar">
-    <div class="container-fluid">
+  <nav id="header" class="navbar pb-md-4 px-md-3 pt-sm-1">
+    <div class="container-fluid align-items-end">
       <a class="navbar-brand" href="#"><img src="./../assets/pyeflex.png" alt=""></a>
 
       <form action="" class="d-none d-md-block">
@@ -9,14 +9,14 @@
       </form>
 
       <div class="d-sm-block d-md-none toggle-icon d-flex flex-wrap">
-        <button v-b-toggle.collapse-1><b-icon icon='search' class="icon"></b-icon></button>
+        <button v-b-toggle.collapse-1><b-icon icon='search' class="icon"  font-scale="1.5"></b-icon></button>
       </div>
-      
+
       <b-collapse id="collapse-1" class="mt-2">
           <!-- <b-card> -->
             <form action="">
               <input type="text" placeholder="">
-              <button type="submit" class="d-none"><b-icon icon='search' class="icon"></b-icon></button>
+              <button type="submit" class="d-none"><b-icon icon='search' class="icon "></b-icon></button>
             </form>
           <!-- </b-card> -->
         </b-collapse>
